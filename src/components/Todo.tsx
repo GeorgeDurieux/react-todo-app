9b1570ea446e6b8a7ed973cfc47e0fd8b8c5874c
George Durieux <giorgosdurieux@gmail.com>
2025-06-02 17:58:26 +0300

    feat: checkbox

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -9,6 +9,7 @@ const toDoReducer = ( state: TodoProps[], action: Action): TodoProps[] => {
             const newTodo: TodoProps = {
                 id: Date.now(),
                 text: action.payload,
+                completed: false
             }
             return [...state,  newTodo]
         case 'DELETE':
@@ -17,6 +18,12 @@ const toDoReducer = ( state: TodoProps[], action: Action): TodoProps[] => {
             return state.map( todo =>
                 todo.id === action.payload.id
                 ? {...todo, text: action.payload.newText}
+                : todo
+            )
+        case 'COMPLETE':
+            return state.map( todo =>
+                todo.id === action.payload
+                ? {...todo, completed: !todo.completed}
                 : todo
             )
         default:
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import {Edit, Save, Trash2, X} from "lucide-react";
+import {CheckSquare, Edit, Save, Square, Trash2, X} from "lucide-react";
 import type { TodoListProps } from "../types.ts";
 import {useState} from "react";
 
@@ -26,13 +26,17 @@ const TodoList = ({todos, dispatch}: TodoListProps) => {
         setEditText('')
     }
 
+    const handleToggle = (id: number) => () => {
+        dispatch({type: "COMPLETE", payload: id})
+    }
+
     return (
         <>
             <ul className="space-y-2">
                 {todos.map(todo => (
                     <li
                         key={todo.id}
-                        className="flex items-center justify-between bg-gray-100 p-2 rounded"
+                        className={`flex items-center justify-between bg-gray-100 p-2 rounded ${todo.completed? "[opacity-60" : ""}`}
                     >
                         {editId === todo.id ? (
                             <>
@@ -63,7 +67,21 @@ const TodoList = ({todos, dispatch}: TodoListProps) => {
                             </>
                         ) : (
                             <>
-                                <span>{todo.text}</span>
+                                <div className="flex items-center gap-2 flex-1">
+                                    <button
+                                        className="text-green-500"
+                                        onClick={handleToggle(todo.id)}
+                                    >
+                                        {todo.completed ? (
+                                            <CheckSquare size={18} />
+                                        ) : (
+                                            <Square size={18}/>
+                                        )}
+
+                                    </button>
+
+                                    <span>{todo.text}</span>
+                                </div>
 
                                 <div className="flex gap-2">
 
